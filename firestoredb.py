4739a62465a14ed09b355bce3b495f46944e754b
Alex Christy <a.christy@ufl.edu>
2023-08-15 11:35:08 -0400

    Switch to firestore

---
+++ firestoredb.py
@@ -0,0 +1,27 @@
+import os
+from firebase_admin import credentials, firestore, initialize_app
+
+class FirestoreClient:
+    
+    def __init__(self):
+        # Get the path to the Firebase Admin SDK service account key JSON file from an environment variable
+        fs_creds_path = os.getenv('FS_CRED_PATH')
+        
+        # Initialize the credentials with the JSON file
+        cred = credentials.Certificate(fs_creds_path)
+        
+        # Initialize the Firebase application with the credentials
+        self.app = initialize_app(cred)
+        
+        # Create the Firestore client
+        self.db = firestore.client(app=self.app)
+
+    def set_document(self, collection_name, document_name, data):
+        """
+        Set the data for a document in a collection
+        :param collection_name: The name of the collection
+        :param document_name: The name of the document
+        :param data: The data to set for the document
+        """
+        doc_ref = self.db.collection(collection_name).document(document_name)
+        doc_ref.set(data)
--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import scraper
 from utils import *
 from terminal import *
-from mongodb import *
 import os
 import sys
 import argparse
 import logging
 from s3_bucket import s3Bucket
 from typing import List
+from firestoredb import FirestoreClient
 
 # List of ENV variables to check
 variablesToCheck = [
-    'MONGO_DB',
-    'MONGO_COLLECTION',
-    'MONGO_HOST',
-    'MONGO_USERNAME',
-    'MONGO_PASSWORD',
+    'FS_CRED_PATH',
     'AWS_BUCKET_NAME',
     'AWS_ACCESS_KEY_ID',
     'AWS_SECRET_ACCESS_KEY',
@@ -25,16 +21,6 @@ variablesToCheck = [
 # Check if all .env variables are set
 try:
     check_env_variables(variablesToCheck)
-    
-    # Load in environment variables from .env file
-    mongoDBName = os.getenv('MONGO_DB')
-    mongoCollectionName = os.getenv('MONGO_COLLECTION')
-    mongoUsername = os.getenv('MONGO_USERNAME')
-    mongoPassword = os.getenv('MONGO_PASSWORD')
-    basePDFDir = os.getenv('PDF_DIR')
-    mongoHost = os.getenv('MONGO_HOST')
-    s3BucketName = os.getenv('AWS_BUCKET_NAME')
-
 except ValueError as e:
     print(e)
     sys.exit(1)
@@ -82,86 +68,79 @@ def main():
     # Prep s3 bucket
     check_s3_pdf_dirs(s3)
 
-    # Intialize MongoDB
-    logging.info('Starting MongoDB.')
-    db = MongoDB(mongoDBName, mongoCollectionName, host=mongoHost, username=mongoUsername, password=mongoPassword)
+    # Connect to Firestore
+    fs = FirestoreClient()
 
-    # Connect to the correct Mongo server
-    if mongoHost == 'localhost':
-        db.connect_local()
-    else:
-        db.connect_atlas()
+    # logging.debug('Starting PDF retrieval process.')
 
-    logging.debug('Starting PDF retrieval process.')
+    # # Set URL to AMC Travel site and scrape Terminal information from it
+    # url = 'https://www.amc.af.mil/AMC-Travel-Site'
+    # listOfTerminals = scraper.get_terminals(url)
 
-    # Set URL to AMC Travel site and scrape Terminal information from it
-    url = 'https://www.amc.af.mil/AMC-Travel-Site'
-    listOfTerminals = scraper.get_terminals(url)
+    # # Populate the DB
+    # for terminal in listOfTerminals:
+    #     db.upsert_terminal(terminal)
 
-    # Populate the DB
-    for terminal in listOfTerminals:
-        db.upsert_terminal(terminal)
+    # # Get links to all the most up to date PDFs on Terminal sites
+    # scraper.get_terminals_pdf_links(db)
 
-    # Get links to all the most up to date PDFs on Terminal sites
-    scraper.get_terminals_pdf_links(db)
+    # # Download all the PDFs for each Terminal and
+    # # save downloaded tmp paths of these PDFs to 
+    # # a list of terminal objects.
+    # listOfTerminals = scraper.download_terminals_pdfs(db)
 
-    # Download all the PDFs for each Terminal and
-    # save downloaded tmp paths of these PDFs to 
-    # a list of terminal objects.
-    listOfTerminals = scraper.download_terminals_pdfs(db)
+    # # Check each PDF directory to confirm something was
+    # # downloaded.
+    # dirs_to_check = [basePDFDir + 'tmp/72_HR/', basePDFDir + 'tmp/30_DAY/', basePDFDir + 'tmp/ROLLCALL/']
+    # successful_downloads = [check_downloaded_pdfs(dir_path) for dir_path in dirs_to_check]
+    # if all(successful_downloads):
+    #     logging.info("PDFs were successfully downloaded in all directories.")
+    # else:
+    #     logging.warning("Some directories did not have successful PDF downloads.")
 
-    # Check each PDF directory to confirm something was
-    # downloaded.
-    dirs_to_check = [basePDFDir + 'tmp/72_HR/', basePDFDir + 'tmp/30_DAY/', basePDFDir + 'tmp/ROLLCALL/']
-    successful_downloads = [check_downloaded_pdfs(dir_path) for dir_path in dirs_to_check]
-    if all(successful_downloads):
-        logging.info("PDFs were successfully downloaded in all directories.")
-    else:
-        logging.warning("Some directories did not have successful PDF downloads.")
+    # # Calc hashes of the tmp downloaded terminal PDFs
+    # # using the temp Terminal objects.
+    # for terminal in listOfTerminals:
+    #     terminal = scraper.calc_terminal_pdf_hashes(terminal)
 
-    # Calc hashes of the tmp downloaded terminal PDFs
-    # using the temp Terminal objects.
-    for terminal in listOfTerminals:
-        terminal = scraper.calc_terminal_pdf_hashes(terminal)
+    # # Check for updates by comparing current terminal objects hashes
+    # # with the stored hashes in MongoDB. If the document does not exist
+    # # like during the first run. is_XXX_updated() will return True.
+    # updatedTerminals = []
+    # for terminal in listOfTerminals:
 
-    # Check for updates by comparing current terminal objects hashes
-    # with the stored hashes in MongoDB. If the document does not exist
-    # like during the first run. is_XXX_updated() will return True.
-    updatedTerminals = []
-    for terminal in listOfTerminals:
+    #     wasUpdated = False
 
-        wasUpdated = False
-
-        if db.is_72hr_updated(terminal):
-            terminal.is72HourUpdated = True
-            wasUpdated = True
-        else:
-            db.set_terminal_field(terminal.name, 'is72HourUpdated', False)
+    #     if db.is_72hr_updated(terminal):
+    #         terminal.is72HourUpdated = True
+    #         wasUpdated = True
+    #     else:
+    #         db.set_terminal_field(terminal.name, 'is72HourUpdated', False)
         
-        if db.is_30day_updated(terminal):
-            terminal.is30DayUpdated = True
-            wasUpdated = True
-        else:
-            db.set_terminal_field(terminal.name, 'is30DayUpdated', False)
+    #     if db.is_30day_updated(terminal):
+    #         terminal.is30DayUpdated = True
+    #         wasUpdated = True
+    #     else:
+    #         db.set_terminal_field(terminal.name, 'is30DayUpdated', False)
 
-        if db.is_rollcall_updated(terminal):
-            terminal.isRollcallUpdated = True
-            wasUpdated = True
-        else:
-            db.set_terminal_field(terminal.name, 'isRollcallUpdated', False)
+    #     if db.is_rollcall_updated(terminal):
+    #         terminal.isRollcallUpdated = True
+    #         wasUpdated = True
+    #     else:
+    #         db.set_terminal_field(terminal.name, 'isRollcallUpdated', False)
         
-        if wasUpdated:
-            updatedTerminals.append(terminal)
+    #     if wasUpdated:
+    #         updatedTerminals.append(terminal)
     
-    # Archive the PDFs that will be replaced updated PDFs
-    archive_pdfs_s3(db, s3, updatedTerminals)
-    rotate_pdfs_to_current_s3(db, s3, updatedTerminals)
+    # # Archive the PDFs that will be replaced updated PDFs
+    # archive_pdfs_s3(db, s3, updatedTerminals)
+    # rotate_pdfs_to_current_s3(db, s3, updatedTerminals)
 
-    # ##################################################
-    # # Place holder for Azure AI Services Upload func #
-    # ##################################################
+    # # ##################################################
+    # # # Place holder for Azure AI Services Upload func #
+    # # ##################################################
     
-    logging.info('Successfully finished program!')
+    # logging.info('Successfully finished program!')
 
 if __name__ == "__main__":
     main()
