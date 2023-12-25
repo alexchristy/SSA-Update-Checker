6cd0508ef476eb9a58239e6a6879750304910591
alexchristy <a.christy@ufl.edu>
2023-12-25 00:48:05 -0500

    Add BWI terminal PDF scrape tests

--- tests/test_pdf_utils.py
+++ tests/test_pdf_utils.py
@@ -14,11 +14,6 @@ from pdf_utils import (  # noqa: E402 (Relative import)
     sort_terminal_pdfs,
     type_pdfs_by_content,
 )
-from terminal import Terminal  # noqa: E402 (Relative import)
-
-load_dotenv()
-terminal = Terminal()
-terminal.link = "https://www.amc.af.mil/AMC-Travel-Site/Terminals/CONUS-Terminals/NS-Norfolk-Passenger-Terminal/"
 
 
 class TestTypePdfsByContent(unittest.TestCase):
@@ -213,3 +208,63 @@ class TestSortTerminalPdfs(unittest.TestCase):
         self.assertEqual(ret_pdf_rollcall, pdf_rollcall)
         self.assertEqual(pdf_aef_72hr.type, "DISCARD")
         self.assertEqual(pdf_gram.type, "DISCARD")
+
+    def test_bwi(self: "TestSortTerminalPdfs") -> None:
+        """Test with BWI terminal PDF scrape."""
+        with open(
+            "tests/assets/TestSortTerminalPdfs/test_bwi/BWI_72HR_24-26DEC23_840971f8-f3c1-48a2-97a1-f3b47871481a.pdf.pkl",
+            "rb",
+        ) as f:
+            pdf_72hr: Pdf = pickle.load(f)  # noqa: S301 (Only for testing)
+
+        if not pdf_72hr:
+            self.fail(
+                "Failed to load 72hr pdf object: tests/assets/TestSortTerminalPdfs/test_bwi/BWI_72HR_24-26DEC23_840971f8-f3c1-48a2-97a1-f3b47871481a.pdf.pkl"
+            )
+
+        with open(
+            "tests/assets/TestSortTerminalPdfs/test_bwi/BWI_30DAY_DEC23JAN24_4503c09a-e469-4798-bbf0-2dfea582720c.pdf.pkl",
+            "rb",
+        ) as f:
+            pdf_30day: Pdf = pickle.load(f)  # noqa: S301 (Only for testing)
+
+        if not pdf_30day:
+            self.fail(
+                "Failed to load 30day pdf object: tests/assets/TestSortTerminalPdfs/test_bwi/BWI_30DAY_DEC23JAN24_4503c09a-e469-4798-bbf0-2dfea582720c.pdf.pkl"
+            )
+
+        with open(
+            "tests/assets/TestSortTerminalPdfs/test_bwi/BWI_ROLLCALL_21DEC23_d1d0ad96-f5b8-46ee-8ed3-04714f122c60.pdf.pkl",
+            "rb",
+        ) as f:
+            pdf_rollcall: Pdf = pickle.load(f)  # noqa: S301 (Only for testing)
+
+        if not pdf_rollcall:
+            self.fail(
+                "Failed to load rollcall pdf object: tests/assets/TestSortTerminalPdfs/test_bwi/BWI_ROLLCALL_21DEC23_d1d0ad96-f5b8-46ee-8ed3-04714f122c60.pdf.pkl"
+            )
+
+        pdfs = [pdf_72hr, pdf_rollcall, pdf_30day]
+
+        ret_pdf_72hr, ret_pdf_30day, ret_pdf_rollcall = sort_terminal_pdfs(pdfs)
+
+        # Ensure all three types were found
+        self.assertIsNotNone(ret_pdf_72hr)
+        self.assertIsNotNone(ret_pdf_30day)
+        self.assertIsNotNone(ret_pdf_rollcall)
+
+        # Set types of the loaded PDFs to make a comparison
+        pdf_72hr.type = "72_HR"
+        pdf_30day.type = "30_DAY"
+        pdf_rollcall.type = "ROLLCALL"
+
+        self.assertEqual(ret_pdf_72hr, pdf_72hr)
+        self.assertEqual(ret_pdf_30day, pdf_30day)
+        self.assertEqual(ret_pdf_rollcall, pdf_rollcall)
+
+    def tearDown(self: "TestSortTerminalPdfs") -> None:
+        """Reset the PDF_DIR environment variable."""
+        if self.old_pdf_dir:
+            os.environ["PDF_DIR"] = self.old_pdf_dir
+        else:
+            os.environ.pop("PDF_DIR", None)
