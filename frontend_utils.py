5b9d4ccd47c3d3dee8b3cd772550df76a739b2dc
Alex Christy <a.christy@ufl.edu>
2024-02-23 10:58:23 -0500

    Added tests and functions to download terminal photos

--- frontend_utils.py
+++ frontend_utils.py
@@ -1,19 +1,28 @@
+from typing import Optional
 import os
-import requests
-from bs4 import BeautifulSoup  # type: ignore
 import urllib.request
+import logging
+from scraper_utils import get_with_retry
+from bs4 import BeautifulSoup  # type: ignore
 
 
-def download_image_from_html(url):
+def get_terminal_image_url(url: str) -> Optional[str]:
+    """
+    Retrieves the image URL from a webpage given its URL.
+
+    Args:
+        url (str): The URL of the webpage.
+
+    Returns:
+        Optional[str]: The image URL or None if the image could not be found.
+    """
     try:
-        # Send an HTTP GET request to the URL
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
+        response = get_with_retry(url)
+        if not response:
+            logging.info("Failed to get a response from %s", url)
+            return None
 
-        # Parse the HTML content
         soup = BeautifulSoup(response.text, "html.parser")
-
-        # Find the <img> tag inside <figure class="hero banner"> and <picture class="fixed-aspect">
         img_tag = (
             soup.find("figure", class_="hero banner")
             .find("picture", class_="fixed-aspect")
@@ -21,16 +30,54 @@ def download_image_from_html(url):
         )
 
         if img_tag and "src" in img_tag.attrs:
-            image_url = img_tag["src"]
-            # Get the image name from the image URL
-            image_name = os.path.basename(image_url)
-
-            # Download the image
-            urllib.request.urlretrieve(image_url, image_name)
-            print(f"Image successfully downloaded: {image_name}")
+            return img_tag["src"]
         else:
-            print("Image tag with 'src' attribute not found.")
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
+            logging.info("Image tag with 'src' attribute not found.")
+            return None
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logging.error(
+            "An error occurred while retrieving the image URL (%s): %s", url, e
+        )
+        return None
+
+
+def download_image(image_url: str, save_path: str) -> bool:
+    """
+    Downloads an image from the specified URL and saves it to the given path.
+
+    Args:
+        image_url (str): The URL of the image to download.
+        save_path (str): The path to save the downloaded image.
+
+    Returns:
+        bool: True if the image was successfully downloaded, False otherwise.
+    """
+    try:
+        urllib.request.urlretrieve(image_url, save_path)
+        logging.info(f"Image successfully downloaded: {save_path}")
+        return True
+    except Exception as e:
+        logging.error(
+            "An error occurred while downloading the image (%s): %s", image_url, e
+        )
+        return False
+
+
+def download_terminal_image(url: str) -> Optional[str]:
+    """
+    Downloads an image from a webpage given its URL. This function uses `get_image_url_from_html`
+    to retrieve the image URL and `download_image` to download the image.
+
+    Args:
+        url (str): The URL of the webpage to download the image from.
+
+    Returns:
+        Optional[str]: The full path to the downloaded image or None if the image could not be downloaded.
+    """
+    image_url = get_terminal_image_url(url)
+    if image_url:
+        image_name = os.path.basename(image_url)
+        full_path = os.path.join(os.getcwd(), image_name)
+        if download_image(image_url, full_path):
+            return full_path
+    return None
---
+++ tests/test_frontend_utils.py
@@ -0,0 +1,219 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from frontend_utils import get_terminal_image_url
+import pickle
+import os
+from firestoredb import FirestoreClient
+from s3_bucket import S3Bucket
+from scraper_utils import get_with_retry
+
+
+class TestGetTerminalImageUrl(unittest.TestCase):
+    """Test that we can find the URL for a terminal's image."""
+
+    terminal_coll: str
+    pdf_archive_coll: str
+    lock_coll: str
+    firestore_cert: str
+    fs: FirestoreClient
+    s3: S3Bucket
+    bwi_page: dict
+    dover_page_no_pdfs: dict
+    andrews_page_no_pdfs: dict
+    seattle_page_no_pdfs: dict
+    charleston_page_no_pdfs: dict
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Set up the test cases for TestGetTerminalImageUrl."""
+        # Create a FirestoreClient object
+        # Set collection names
+        cls.terminal_coll = "**TestGetTerminalImageUrl**_Terminals"
+        cls.pdf_archive_coll = "**TestGetTerminalImageUrl**_PDF_Archive"
+        cls.lock_coll = "**TestGetTerminalImageUrl**_Locks"
+        cls.firestore_cert = "./creds.json"
+
+        os.environ["TERMINAL_COLL"] = cls.terminal_coll
+        os.environ["PDF_ARCHIVE_COLL"] = cls.pdf_archive_coll
+        os.environ["LOCK_COLL"] = cls.lock_coll
+        os.environ["FS_CRED_PATH"] = cls.firestore_cert
+
+        cls.fs = FirestoreClient()
+        cls.s3 = S3Bucket()
+
+        # Load the serialized response
+        with open(
+            "tests/assets/TestGetTerminalImageUrl/bwi_page_02-17-2024.pkl",
+            "rb",
+        ) as file:
+            cls.bwi_page = pickle.load(file)  # noqa: S301 (Loading test data)
+
+        with open(
+            "tests/assets/TestGetTerminalImageUrl/dover_page_02-17-24_NO_PDFS.pkl",
+            "rb",
+        ) as file:
+            cls.dover_page_no_pdfs = pickle.load(file)  # noqa: S301 (Loading test data)
+
+        with open(
+            "tests/assets/TestGetTerminalImageUrl/andrews_page_02-17-24_NO_PDFS.pkl",
+            "rb",
+        ) as file:
+            cls.andrews_page_no_pdfs = pickle.load(  # noqa: S301 (Loading test data)
+                file
+            )
+
+        with open(
+            "tests/assets/TestGetTerminalImageUrl/seattle_page_02-17-24_NO_PDFS.pkl",
+            "rb",
+        ) as file:
+            cls.seattle_page_no_pdfs = pickle.load(  # noqa: S301 (Loading test data)
+                file
+            )
+
+        with open(
+            "tests/assets/TestGetTerminalImageUrl/charleston_page_02-17-24_NO_PDFS.pkl",
+            "rb",
+        ) as file:
+            cls.charleston_page_no_pdfs = pickle.load(
+                file
+            )  # noqa: S301 (Loading test data)
+
+    @patch("frontend_utils.get_with_retry")
+    def test_get_terminal_image_url_bwi(
+        self: "TestGetTerminalImageUrl", mock_get_with_retry: MagicMock
+    ) -> None:
+        """Test that we can find the URL for a terminal's image."""
+        # Prepare the mock response object
+        mock_response = MagicMock()
+        mock_response.status_code = self.bwi_page["status_code"]
+        mock_response.headers = self.bwi_page["headers"]
+        # Assuming the content is pickled as bytes, decode to simulate response.text
+        mock_response.text = self.bwi_page["content"].decode("utf-8")
+
+        # Set mock object to return the mock response
+        mock_get_with_retry.return_value = mock_response
+
+        # Actual URL used is not critical due to mocking; adjust as needed
+        url = "https://example.com"
+        expected_image_url = "https://media.defense.gov/2022/Jan/11/2002919982/-1/-1/0/200122-F-F3200-002.JPG"  # Adjust based on your expectations
+
+        # Call the function under test
+        found_image_url = get_terminal_image_url(url)
+
+        # Verify the function returns the expected URL
+        self.assertEqual(found_image_url, expected_image_url)
+
+        # Additional assertions can be made here, for example, checking call count
+        mock_get_with_retry.assert_called_once_with(url)
+
+    @patch("frontend_utils.get_with_retry")
+    def test_get_terminal_image_url_dover(
+        self: "TestGetTerminalImageUrl", mock_get_with_retry: MagicMock
+    ) -> None:
+        """Test that we can find the URL for a terminal's image."""
+        # Prepare the mock response object
+        mock_response = MagicMock()
+        mock_response.status_code = self.dover_page_no_pdfs["status_code"]
+        mock_response.headers = self.dover_page_no_pdfs["headers"]
+        # Assuming the content is pickled as bytes, decode to simulate response.text
+        mock_response.text = self.dover_page_no_pdfs["content"].decode("utf-8")
+
+        # Set mock object to return the mock response
+        mock_get_with_retry.return_value = mock_response
+
+        # Actual URL used is not critical due to mocking; adjust as needed
+        url = "https://example.com"
+        expected_image_url = "https://media.defense.gov/2022/Mar/07/2002951829/-1/-1/0/070322-F-F3200-001.JPG"  # Adjust based on your expectations
+
+        # Call the function under test
+        found_image_url = get_terminal_image_url(url)
+
+        # Verify the function returns the expected URL
+        self.assertEqual(found_image_url, expected_image_url)
+
+        # Additional assertions can be made here, for example, checking call count
+        mock_get_with_retry.assert_called_once_with(url)
+
+    @patch("frontend_utils.get_with_retry")
+    def test_get_terminal_image_url_andrews(
+        self: "TestGetTerminalImageUrl", mock_get_with_retry: MagicMock
+    ) -> None:
+        """Test that we can find the URL for a terminal's image."""
+        # Prepare the mock response object
+        mock_response = MagicMock()
+        mock_response.status_code = self.andrews_page_no_pdfs["status_code"]
+        mock_response.headers = self.andrews_page_no_pdfs["headers"]
+        # Assuming the content is pickled as bytes, decode to simulate response.text
+        mock_response.text = self.andrews_page_no_pdfs["content"].decode("utf-8")
+
+        # Set mock object to return the mock response
+        mock_get_with_retry.return_value = mock_response
+
+        # Actual URL used is not critical due to mocking; adjust as needed
+        url = "https://example.com"
+        expected_image_url = "https://media.defense.gov/2022/Feb/02/2002931758/-1/-1/0/020222-F-F3200-002.JPG"  # Adjust based on your expectations
+
+        # Call the function under test
+        found_image_url = get_terminal_image_url(url)
+
+        # Verify the function returns the expected URL
+        self.assertEqual(found_image_url, expected_image_url)
+
+        # Additional assertions can be made here, for example, checking call count
+        mock_get_with_retry.assert_called_once_with(url)
+
+    @patch("frontend_utils.get_with_retry")
+    def test_get_terminal_image_url_seattle(
+        self: "TestGetTerminalImageUrl", mock_get_with_retry: MagicMock
+    ) -> None:
+        """Test that we can find the URL for a terminal's image."""
+        # Prepare the mock response object
+        mock_response = MagicMock()
+        mock_response.status_code = self.seattle_page_no_pdfs["status_code"]
+        mock_response.headers = self.seattle_page_no_pdfs["headers"]
+        # Assuming the content is pickled as bytes, decode to simulate response.text
+        mock_response.text = self.seattle_page_no_pdfs["content"].decode("utf-8")
+
+        # Set mock object to return the mock response
+        mock_get_with_retry.return_value = mock_response
+
+        # Actual URL used is not critical due to mocking; adjust as needed
+        url = "https://example.com"
+        expected_image_url = "https://media.defense.gov/2023/Aug/29/2003290861/-1/-1/0/230829-F-DJ189-1001.JPG"  # Adjust based on your expectations
+
+        # Call the function under test
+        found_image_url = get_terminal_image_url(url)
+
+        # Verify the function returns the expected URL
+        self.assertEqual(found_image_url, expected_image_url)
+
+        # Additional assertions can be made here, for example, checking call count
+        mock_get_with_retry.assert_called_once_with(url)
+
+    @patch("frontend_utils.get_with_retry")
+    def test_get_terminal_image_url_charleston(
+        self: "TestGetTerminalImageUrl", mock_get_with_retry: MagicMock
+    ) -> None:
+        """Test that we can find the URL for a terminal's image."""
+        # Prepare the mock response object
+        mock_response = MagicMock()
+        mock_response.status_code = self.charleston_page_no_pdfs["status_code"]
+        mock_response.headers = self.charleston_page_no_pdfs["headers"]
+        # Assuming the content is pickled as bytes, decode to simulate response.text
+        mock_response.text = self.charleston_page_no_pdfs["content"].decode("utf-8")
+
+        # Set mock object to return the mock response
+        mock_get_with_retry.return_value = mock_response
+
+        # Actual URL used is not critical due to mocking; adjust as needed
+        url = "https://example.com"
+        expected_image_url = "https://media.defense.gov/2023/Jan/13/2003144784/-1/-1/0/230110-F-XY111-1001.JPG"  # Adjust based on your expectations
+
+        # Call the function under test
+        found_image_url = get_terminal_image_url(url)
+
+        # Verify the function returns the expected URL
+        self.assertEqual(found_image_url, expected_image_url)
+
+        # Additional assertions can be made here, for example, checking call count
+        mock_get_with_retry.assert_called_once_with(url)
