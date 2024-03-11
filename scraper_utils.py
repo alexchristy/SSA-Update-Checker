d026543482ca61e1e3cf5c4178e63241631dd05d
Alex Christy <a.christy@ufl.edu>
2024-03-11 09:06:41 -0400

    Downgrade request timeouts to warnings

--- scraper_utils.py
+++ scraper_utils.py
@@ -255,7 +255,7 @@ def get_with_retry(url: str) -> Optional[requests.Response]:
             return response
 
         except requests.Timeout:
-            logging.error("Request to %s timed out.", url)
+            logging.warning("Request to %s timed out.", url)
 
         except Exception as e:  # Catch any exceptions
             logging.warning("Request to %s failed in get_with_retry().", url)
